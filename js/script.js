462fb568f45c832785a346277f42c2c091598df4
ArtPlatosha <zipex98@gmail.com>
2023-05-02 02:23:39 +0300

    commit: 25th commit
    adding burger (JS)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ let login = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () => {
 	login.classList.toggle('active');
+	navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+	login.classList.remove('active');
+	navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+	login.classList.remove('active');
+	navbar.classList.remove('active');
 }
 
 var swiper = new Swiper('.gallery-slider', {
