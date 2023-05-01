e6bb3310ef40f34e55c7953e5f30dce756b55326
ArtPlatosha <zipex98@gmail.com>
2023-05-01 22:31:08 +0300

    commit: 13th commit
    adding galley-section (JS)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 let login = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () => {
-	login.classList.toggle('active')
+	login.classList.toggle('active');
 }
+
+var swiper = new Swiper('.gallery-slider', {
+	grabCursor: true,
+	loop: true,
+	centeredSlides: true,
+	spaceBetween:20,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		700: {
+			slidesPerView: 2,
+		}
+	}
+})
